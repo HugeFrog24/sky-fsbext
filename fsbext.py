040603b64395c796da265c22ecf67e50140bdb66
HugeFrog24 <bogerserge@gmail.com>
2023-03-19 16:50:51 -0700

    Split code into logical components

--- fsbext.py
+++ fsbext.py
@@ -1,10 +1,10 @@
 import argparse
 import platform
-import os
 import shutil
 import subprocess
 import sys
 import logging
+from pathlib import Path
 
 __author__ = "Tibik"
 __version__ = "1.0.1"
@@ -12,9 +12,11 @@ __version__ = "1.0.1"
 # Define the minimum required disk space (in bytes)
 MIN_DISK_SPACE = 7 * 1024 * 1024 * 1024
 
+
 class ConsoleFilter(logging.Filter):
     def filter(self, record):
         return not getattr(record, "log_to_file_only", False)
+
 
 # Set up root logger
 root_logger = logging.getLogger()
@@ -36,14 +38,7 @@ root_logger.addHandler(console_handler)
 LOGGER_PADDING = '=' * 10
 
 
-def main(args):
-    version_str = f"SKY-FSBEXT version: {__version__} by {__author__}"
-    root_logger.info(f"{LOGGER_PADDING} {version_str} {LOGGER_PADDING}")
-    root_logger.info(f"Operating system: {platform.system()} {platform.release()}")
-    if args.version:
-        print(version_str)
-        exit()
-
+def check_disk_space():
     # Check available disk space
     free_space = shutil.disk_usage(".").free
     if free_space < MIN_DISK_SPACE:
@@ -52,88 +47,109 @@ def main(args):
             f"({free_space / (1024 * 1024 * 1024):.2f} GB)"
         )
 
+
+def create_directory_structure(output_dir: Path):
+    (output_dir / "Music").mkdir(parents=True, exist_ok=True)
+    (output_dir / "SFX").mkdir(parents=True, exist_ok=True)
+    (output_dir / "Other").mkdir(parents=True, exist_ok=True)
+    root_logger.info("Created directory structure.")
+
+
+def remove_empty_directories(output_dir: Path):
+    for dir_path in output_dir.glob('**/*'):
+        if dir_path.is_dir() and not any(dir_path.iterdir()):
+            shutil.rmtree(dir_path)
+            root_logger.info(f"Removed empty directory: {dir_path}", extra={"log_to_file_only": True})
+
+
+def extract_and_move_files(args, bank_files):
+    extracted_files = 0
+    for bank_file in bank_files:
+        # Determine the output directory
+        if bank_file.startswith("Music_"):
+            bank_dir = args.output_dir / "Music" / bank_file[:-5]
+        elif bank_file.startswith("SFX_"):
+            bank_dir = args.output_dir / "SFX"
+        else:
+            bank_dir = args.output_dir / "Other" / bank_file[:-5]
+
+        # Create the output directory if it doesn't exist
+        bank_dir.mkdir(parents=True, exist_ok=True)
+        root_logger.info(f"Created output directory: {bank_dir}", extra={"log_to_file_only": True})
+
+        # Extract the bank file to WAV files
+        status = None
+        err_reason = ""
+        try:
+            subprocess.run(
+                [
+                    args.vgmstream_path, args.input_dir / bank_file,
+                    "-o", bank_dir / "?n.wav", "-S", "0"
+                ], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
+            )
+        except subprocess.CalledProcessError as e:
+            status = False
+            err_reason = e
+        else:
+            status = True
+            extracted_files += 1
+        finally:
+            root_logger.info(
+                f"Processing {bank_file}: {'OK!' if status else 'ERR!'}"
+            )
+            if not status:
+                root_logger.error(f"An error occurred while extracting {bank_file}: {err_reason}",
+                                  extra={"log_to_file_only": True})
+
+            # Check if the output directory is empty and remove it if it is
+            if not any(bank_dir.iterdir()):
+                shutil.rmtree(bank_dir)
+                root_logger.info(f"Removed empty directory: {bank_dir}", extra={"log_to_file_only": True})
+
+    return extracted_files
+
+
+def main(args):
+    version_str = f"SKY-FSBEXT version: {__version__} by {__author__}"
+    root_logger.info(f"{LOGGER_PADDING} {version_str} {LOGGER_PADDING}")
+    root_logger.info(f"Operating system: {platform.platform()}")
+    if args.version:
+        print(version_str)
+        exit()
+    check_disk_space()
+
     # Log input and output directories
     root_logger.info(f"Input directory: {args.input_dir}")
     root_logger.info(f"Output directory: {args.output_dir}")
 
-    # Create the directory structure
-    os.makedirs(os.path.join(args.output_dir, "Music"), exist_ok=True)
-    os.makedirs(os.path.join(args.output_dir, "SFX"), exist_ok=True)
-    os.makedirs(os.path.join(args.output_dir, "Other"), exist_ok=True)
-    root_logger.info("Created directory structure")
+    create_directory_structure(args.output_dir)
 
     # Check if the "in" directory exists and rebuild it if necessary
-    if not os.path.isdir(args.input_dir):
-        os.makedirs(args.input_dir)
+    if not args.input_dir.is_dir():
+        args.input_dir.mkdir(parents=True)
         root_logger.warning("Input directory not found - rebuilding")
 
     # Search for .bank files in the in directory
-    bank_files = [f for f in os.listdir(args.input_dir) if f.endswith(".bank")]
+    bank_files = [f for f in args.input_dir.glob("*.bank")]
+
     if not bank_files:
         root_logger.warning("No sound banks found in input directory")
     else:
         root_logger.info(f"Found {len(bank_files)} sound bank(s) in input directory")
 
-        # Check if the vgmstream executable is present and get its version number
-        if not os.path.isfile(args.vgmstream_path):
-            root_logger.error("vgmstream-cli executable not found")
-            exit(1)
+    # Check if the vgmstream executable is present and get its version number
+    if not args.vgmstream_path.is_file():
+        root_logger.error("vgmstream-cli executable not found")
+        exit(1)
 
-        # Extract and move the files
-        extracted_files = 0
-        for bank_file in bank_files:
-            # Determine the output directory
-            if bank_file.startswith("Music_"):
-                bank_dir = os.path.join(args.output_dir, "Music", bank_file[:-5])
-            elif bank_file.startswith("SFX_"):
-                bank_dir = os.path.join(args.output_dir, "SFX")
-            else:
-                bank_dir = os.path.join(args.output_dir, "Other", bank_file[:-5])
+    extracted_files = extract_and_move_files(args, bank_files)
 
-            # Create the output directory if it doesn't exist
-            os.makedirs(bank_dir, exist_ok=True)
-            root_logger.info(f"Created output directory: {bank_dir}", extra={"log_to_file_only": True})
+    if extracted_files > 0:
+        root_logger.info(f"Successfully extracted {extracted_files} bank file(s)")
+    else:
+        root_logger.warning("No sound banks were extracted")
 
-            # Extract the bank file to WAV files
-            status = None
-            err_reason = ""
-            try:
-                subprocess.run(
-                    [
-                        args.vgmstream_path, os.path.join(args.input_dir, bank_file),
-                        "-o", os.path.join(bank_dir, "?n.wav"), "-S", "0"
-                    ], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL
-                )
-            except subprocess.CalledProcessError as e:
-                status = False
-                err_reason = e
-            else:
-                status = True
-                extracted_files += 1
-            finally:
-                root_logger.info(
-                    f"Processing {bank_file}: {'OK!' if status else 'ERR!'}"
-                )
-                if not status:
-                    root_logger.error(f"An error occurred while extracting {bank_file}: {err_reason}", extra={"log_to_file_only": True})
-
-                # Check if the output directory is empty and remove it if it is
-                if not os.listdir(bank_dir):
-                    os.rmdir(bank_dir)
-                    root_logger.info(f"Removed empty directory: {bank_dir}", extra={"log_to_file_only": True})
-
-        if extracted_files > 0:
-            root_logger.info(f"Successfully extracted {extracted_files} bank file(s)")
-        else:
-            root_logger.warning("No sound banks were extracted")
-
-        # Remove empty directories
-        for root, dirs, files in os.walk("out", topdown=False):
-            for directory in dirs:
-                dir_path = os.path.join(root, directory)
-                if not os.listdir(dir_path):
-                    os.rmdir(dir_path)
-                    root_logger.info(f"Removed empty directory: {dir_path}", extra={"log_to_file_only": True})
+    remove_empty_directories(args.output_dir)
 
 
 if __name__ == "__main__":
@@ -146,10 +162,10 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "vgmstream_path", nargs="?", default="vgmstream-win64/vgmstream-cli.exe",
-        help="Path to vgmstream-cli executable."
+        type=Path, help="Path to vgmstream-cli executable."
     )
-    parser.add_argument("-i", "--input-dir", default="in", help="Path to the input directory.")
-    parser.add_argument("-o", "--output-dir", default="out", help="Path to the output directory.")
+    parser.add_argument("-i", "--input-dir", default="in", type=Path, help="Path to the input directory.")
+    parser.add_argument("-o", "--output-dir", default="out", type=Path, help="Path to the output directory.")
     parser.add_argument("-v", "--version", action="store_true", help="Prints script version.")
     parser.add_argument("-V", "--verbose", action="store_true", help="Enable verbose output.")
     parsed_args = parser.parse_args()
